afd7d937ec066b9de68947ea359570af9c672820
Tomer Fidel <tomerfid97@gmail.com>
2020-05-28 16:54:59 +0300

    remove .env file

--- server/app.js
+++ server/app.js
@@ -1,9 +1,15 @@
+// Config
 require("dotenv").config();
+// End config
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var DBConnection = require('./helpers/db');
+var connection = new DBConnection();
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
